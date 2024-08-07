306ab3feb8d080fa8049aaabd342a40232c01ff5
Jack Coughlin <jack@johnbcoughlin.com>
2024-08-07 08:56:59 -0700

    Don't print out debug statement

--- docs/scripts/build_params_doc_pages.sh
+++ docs/scripts/build_params_doc_pages.sh
@@ -4,9 +4,13 @@ set -e
 
 TMP=$(mktemp -d)
 
-./warpii --print-parameters ../../examples/five-moment/shu_osher_shock_1d.inp > /dev/null
+./warpii --print-parameters ../../examples/five-moment/shu_osher_shock_1d.inp > $TMP/params.json
 
-./warpii --print-parameters ../../examples/five-moment/shu_osher_shock_1d.inp | docs/scripts/process_params_json.py ../../examples/five-moment/shu_osher_shock_1d.inp > $TMP/contents.html
+cat $TMP/params.json
+
+cat $TMP/params.json | docs/scripts/process_params_json.py ../../examples/five-moment/shu_osher_shock_1d.inp > $TMP/contents.html
+#./warpii --print-parameters ../../examples/five-moment/shu_osher_shock_1d.inp | docs/scripts/process_params_json.py ../../examples/five-moment/shu_osher_shock_1d.inp > $TMP/contents.html
+#./warpii --print-parameters ../../examples/five-moment/shu_osher_shock_1d.inp
 python3 docs/scripts/replace_doxygen_html_contents.py html/five_moment_params.html $TMP/contents.html > $TMP/five_moment_params.html
 
 mv $TMP/five_moment_params.html html/five_moment_params.html
