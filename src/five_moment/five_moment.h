5174e3104e89c1fb43896d25a18431fc6a5b138e
Jack Coughlin <jack@johnbcoughlin.com>
2024-09-11 11:35:03 -0700

    Diagnostic fixup

--- src/five_moment/five_moment.h
+++ src/five_moment/five_moment.h
@@ -83,6 +83,8 @@ class FiveMomentApp : public AbstractApp {
 
     void output_results(const unsigned int result_number);
 
+    void append_diagnostics(const double t, const bool with_header=false);
+
    private:
     std::shared_ptr<five_moment::Extension<dim>> extension;
     std::shared_ptr<NodalDGDiscretization<dim>> discretization;
@@ -233,6 +235,7 @@ void FiveMomentApp<dim>::setup(WarpiiOpts) {
     solver->reinit();
     solver->project_initial_condition();
     output_results(0);
+    append_diagnostics(0.0, true);
 }
 
 template <int dim>
@@ -244,7 +247,13 @@ void FiveMomentApp<dim>::run(WarpiiOpts) {
     // skip the zeroth writeout because we already did that in the setup phase
     TimestepCallback writeout_callback = TimestepCallback(writeout_interval, writeout, false);
 
-    solver->solve(writeout_callback);
+    double diagnostic_interval = writeout_interval / 10.0;
+    auto diagnostic = [&](double t) -> void {
+        append_diagnostics(t);
+    };
+    TimestepCallback diagnostic_callback = TimestepCallback(diagnostic_interval, diagnostic, false);
+
+    solver->solve(writeout_callback, diagnostic_callback);
 }
 
 template <int dim>
@@ -322,6 +331,28 @@ void FiveMomentApp<dim>::output_results(const unsigned int result_number) {
     const std::string filename =
         "solution_" + Utilities::int_to_string(result_number) + ".vtu";
     data_out.write_vtu_in_parallel(filename, MPI_COMM_WORLD);
+}
+
+template <int dim>
+void FiveMomentApp<dim>::append_diagnostics(const double time, const bool with_header) {
+    if (with_header) {
+        std::ofstream file("diagnostics.csv");
+        AssertThrow(file.is_open(), ExcMessage("Could not open diagnostics file"));
+        std::string header_string = "time";
+        if (fields_enabled) {
+            header_string += ",electrostatic_energy";
+        }
+        file << header_string << std::endl;
+    }
+
+    std::ofstream file("diagnostics.csv", std::ios::app);
+    AssertThrow(file.is_open(), ExcMessage("Could not open diagnostics file"));
+    file << time;
+    if (fields_enabled) {
+        file << "," << get_solution_helper()
+            .compute_global_electrostatic_energy(get_solution().mesh_sol);
+    }
+    file << std::endl;
 }
 
 }  // namespace five_moment
